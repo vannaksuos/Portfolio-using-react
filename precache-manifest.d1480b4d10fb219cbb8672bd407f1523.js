316466f3a2195d9b0486989f0c565097249cc749
Vannak <vannaksuos@gmail.com>
2020-06-15 11:45:43 -0400

    Updates

---
+++ precache-manifest.d1480b4d10fb219cbb8672bd407f1523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681052dac1d35d33a94148df5ebc5716",
+    "url": "/Portfolio-using-react/index.html"
+  },
+  {
+    "revision": "aee448f7bb43b45003d3",
+    "url": "/Portfolio-using-react/static/css/main.02adda52.chunk.css"
+  },
+  {
+    "revision": "81f4513eb5ce892fc025",
+    "url": "/Portfolio-using-react/static/js/2.4098e763.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-using-react/static/js/2.4098e763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee448f7bb43b45003d3",
+    "url": "/Portfolio-using-react/static/js/main.5d4e6763.chunk.js"
+  },
+  {
+    "revision": "d73a536bd6df283d0767",
+    "url": "/Portfolio-using-react/static/js/runtime-main.1852a512.js"
+  }
+]);
